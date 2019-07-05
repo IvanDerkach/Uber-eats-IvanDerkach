499ccf52f2ea3c4de7be9c4a33678bc93660c615
Ivan Derkach <horovik88@gmail.com>
2019-07-05 18:24:03 +0300

    restaurant presentation fixed

--- src/components/RestaurantPage/RestaurantPresentation/RestaurantPresentation.jsx
+++ src/components/RestaurantPage/RestaurantPresentation/RestaurantPresentation.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import "./RestaurantPresentation.css";
+import { restaurant } from "../restaurantMenuInfo.js";
 
 export function RestaurantPresentation() {
   return (
-    <div className="restaurantPresentation-card__background">
-      <div className="restaurantPresentation-card__wrapper">
-        <div className="restaurantPresentation-card">
-          <span className="restaurantPresentation-card__title">
-            Трактир «Пушкин»
+    <div className="restaurantPresentation-card__wrapper">
+      <img
+        className="restaurantPresentation__background"
+        src={restaurant.largeImageUrl}
+        alt="restaurant food"
+      />
+      <div className="restaurantPresentation-card">
+        <span className="restaurantPresentation-card__title">
+          {restaurant.title}
+        </span>
+        <div className="restaurantPresentation-card__footer">
+          <span className="restaurantPresentation-card__price">
+            {restaurant.priceBucket} •{" "}
+            {restaurant.categories.map(category => {
+              return category.name;
+            })}
           </span>
-          <div className="restaurantPresentation-card__footer">
-            <span className="restaurantPresentation-card__price">
-              ₴₴₴ • Европейская
-            </span>
-            <span className="restaurantPresentation-card__time">
-              40 - 50 Min
-            </span>
-          </div>
+          <span className="restaurantPresentation-card__time">
+            {restaurant.etaRange.min} - {restaurant.etaRange.max} min
+          </span>
         </div>
       </div>
     </div>
--- src/components/RestaurantPage/restaurantMenuInfo.js
+++ src/components/RestaurantPage/restaurantMenuInfo.js
@@ -155,7 +155,7 @@ export const restaurant = {
     raw: 35
   },
   imageUrl:
-    "https://uber-eats-mates.herokuapp.com/images/ed032859-05fa-4761-9a4f-ac5fb7d5859a.jpg.jpg",
+    "https://uber-eats-mates.herokuapp.com/images/ed032859-05fa-4761-9a4f-ac5fb7d5859a.jpg",
   endorsement: "Найпопулярніші янтики та чебуреки",
   tags: [
     { uuid: "MiddleEastern", name: "Близькосхідна", keyName: "MiddleEastern" },
@@ -172,7 +172,7 @@ export const restaurant = {
     ratingCount: "500+"
   },
   largeImageUrl:
-    "https://uber-eats-mates.herokuapp.com/images/48f30dc6-245f-4257-9b6f-8fc00c510fd9.jpg.jpg",
+    "https://uber-eats-mates.herokuapp.com/images/48f30dc6-245f-4257-9b6f-8fc00c510fd9.jpg",
   items: {
     "6f5a0c7a-b401-4f78-ac3b-0de5cea3fc1c": {
       uuid: "6f5a0c7a-b401-4f78-ac3b-0de5cea3fc1c",
